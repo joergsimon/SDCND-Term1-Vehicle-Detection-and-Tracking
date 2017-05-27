0b03651cc3426c9ce05f300d9bea64d2d99f7f4c
Joerg Simon <jsimon@know-center.at>
2017-05-27 22:04:54 +0200

    added more classifiers and a loop to train them/see what happens

--- helper/classification.py
+++ helper/classification.py
@@ -5,3 +5,54 @@ def scale_features(features):
     # Apply the scaler to X
     scaled_X = X_scaler.transform(features)
     return scaled_X, X_scaler
+
+# this code is taken from my master thesis and will appear there again ^_^
+def get_clf_candidates():
+    import sklearn.linear_model as lm
+    import sklearn.neighbors.nearest_centroid as nc
+    import sklearn.naive_bayes as nb
+    import sklearn.ensemble as em
+    from sklearn import svm
+    from sklearn import tree
+    classifiers = [("svc[ovo]", svm.SVC(decision_function_shape='ovo')),
+                   ("svc", svm.SVC()),
+                   ("lin svc", svm.LinearSVC()),
+                   ("lr", lm.LogisticRegression()),
+                   ("nn", nc.NearestCentroid()),
+                   ("lr(l1)", lm.LogisticRegression(penalty='l1')),
+                   ("sgd[hinge]", lm.SGDClassifier(loss="hinge", penalty="l2")),
+                   ("navie bayse", nb.MultinomialNB()),
+                   ("decision tree", tree.DecisionTreeClassifier()),
+                   ("random forrest", em.RandomForestClassifier(n_estimators=10)),
+                   ("ada boost", em.AdaBoostClassifier(n_estimators=100)),
+                   ("gradient boost",
+                    em.GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=0))]
+    return classifiers
+
+# this code is taken from my master thesis and will appear there again ^_^
+def fit_classifier(values, labels, classifiers):
+    import sklearn.model_selection as ms
+    from sklearn.metrics import accuracy_score
+    clf = None
+    clf_n = None
+    kf = ms.KFold(n_splits=5)
+    result = []
+    for clf_name, clf_candidate in classifiers:
+        print("KFold fit {}".format(clf_name))
+        # decide: we should return the best model for each classifier here?
+        # and do we pickle them?
+        s = 0
+        for train_index, test_index in kf.split(values):
+            X_train = values[train_index, :]
+            X_ct = values[test_index, :]
+            y_train = labels[train_index]
+            y_ct = labels[test_index]
+            clf_candidate.fit(X_train, y_train)
+            p = clf_candidate.predict(X_ct)
+            s += accuracy_score(y_ct, p)
+            print('fold accuracy: {:.3f}'.format(s))
+        s /= 5.0
+        print(clf_name, ' has {:.3f}'.format(s))
+        result.append((clf_name, clf_candidate, s))
+
+    return result
--- train_clf.py
+++ train_clf.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 from helper.io import read_data_set
 from helper.feature_extrection import data_look, extract_features
-from helper.classification import scale_features
+from helper.classification import scale_features, get_clf_candidates, fit_classifier
 
 vehicle_files, non_vehicle_files = read_data_set("./data")
 print(data_look(vehicle_files, non_vehicle_files))
@@ -24,15 +24,19 @@ y = np.hstack((np.ones(len(vehicle_features)), np.zeros(len(non_vehicle_features
 rand_state = np.random.randint(0, 100)
 X_train, X_test, y_train, y_test = train_test_split(
     scaled_X, y, test_size=0.2, random_state=rand_state)
-clf = linear.LassoCV()
-clf.fit(X_train, y_train)
-pred = clf.predict(X_test)
-print(classification_report(y_test, pred))
-print('')
-print('---------------------------------------')
-print(confusion_matrix(y_test, pred))
 
-with open('svc.p', 'wb') as f:
-    pickle.dump(clf, f)
-with open('scaler.p', 'wb') as f:
-    pickle.dump(X_scaler, f)
+clfs = get_clf_candidates()
+fit_all = fit_classifier(X_train, y_train, clfs)
+for name, clf, score in fit_all:
+    pred = clf.predict(X_test)
+    print('test clf ', name)
+    print('---------------------------------------')
+    print(classification_report(y_test, pred))
+    print('')
+    print('---------------------------------------')
+    print(confusion_matrix(y_test, pred))
+
+#with open('svc.p', 'wb') as f:
+#    pickle.dump(clf, f)
+#with open('scaler.p', 'wb') as f:
+#    pickle.dump(X_scaler, f)
