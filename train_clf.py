5235c70ca0d01fc8c36d97ec0549ac25ee030869
Joerg Simon <j_simon@mac.com>
2017-05-27 21:24:47 +0200

    work on the classifier; he must be overfitting

--- train_clf.py
+++ train_clf.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pickle
 from sklearn.svm import LinearSVC
+import sklearn.linear_model as linear
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
 from helper.io import read_data_set
 from helper.feature_extrection import data_look, extract_features
 from helper.classification import scale_features
@@ -22,11 +24,15 @@ y = np.hstack((np.ones(len(vehicle_features)), np.zeros(len(non_vehicle_features
 rand_state = np.random.randint(0, 100)
 X_train, X_test, y_train, y_test = train_test_split(
     scaled_X, y, test_size=0.2, random_state=rand_state)
-svc = LinearSVC()
-svc.fit(X_train, y_train)
-print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+clf = linear.LassoCV()
+clf.fit(X_train, y_train)
+pred = clf.predict(X_test)
+print(classification_report(y_test, pred))
+print('')
+print('---------------------------------------')
+print(confusion_matrix(y_test, pred))
 
 with open('svc.p', 'wb') as f:
-    pickle.dump(svc, f)
+    pickle.dump(clf, f)
 with open('scaler.p', 'wb') as f:
     pickle.dump(X_scaler, f)
